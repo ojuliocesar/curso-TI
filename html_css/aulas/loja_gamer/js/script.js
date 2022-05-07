d8d95edd67f8ef9db50342d30dbf3e909bc8178f
ojuliocesar <97842753+ojuliocesar@users.noreply.github.com>
2022-05-07 02:01:00 -0300

    Update folder

--- html_css/aulas/loja_gamer/js/script.js
+++ html_css/aulas/loja_gamer/js/script.js
@@ -1,3 +1,8 @@
 const myCarousel = new Carousel(document.querySelector("#myCarousel"), {
     // Your options go here
   });
+
+function exibeMenu() {
+  // Exibe/oculta o Menu Mobile(hamburguer)
+  $('#exibe-menu').toggle()
+}
