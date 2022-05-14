447c2a90fd8bca1fa2f6fe45b50bd4e1ed3e7990
ojuliocesar <97842753+ojuliocesar@users.noreply.github.com>
2022-05-13 21:19:25 -0300

    Finish responsive project loja_gamer

--- html_css/aulas/loja_gamer/js/script.js
+++ html_css/aulas/loja_gamer/js/script.js
@@ -1,8 +1,10 @@
+// Inicializa o Carousel
 const myCarousel = new Carousel(document.querySelector("#myCarousel"), {
     // Your options go here
-  });
+});
 
-function exibeMenu() {
-  // Exibe/oculta o Menu Mobile(hamburguer)
-  $('#exibe-menu').toggle()
+// Função que oculta e exibe o menu
+const exibeMenu = () => {
+  // muda a visualização dos itens com a clsse .oculta-navbar, mudando de hide(oculto) para show (exibir).
+  $('.oculta-navbar') .toggle()
 }
