945e9734a0dec9d05464d562bff3d8e342eb72d0
ojuliocesar <97842753+ojuliocesar@users.noreply.github.com>
2022-05-27 21:32:46 -0300

    Finish pizzaria project

--- html_css/aulas/pizzaria/js/script.js
+++ html_css/aulas/pizzaria/js/script.js
@@ -1,10 +1,3 @@
-// $('').waypoint(
-//     function() {
-
-//     }, {
-
-// })
-
 $('section#final-semana').waypoint(
     function(direcao) {
         if (direcao == "down") {
@@ -24,8 +17,70 @@ $('section#final-semana').waypoint(
         }
     }, {
         // Indica quantos pixels antes do elemento o efeito começará a ser aplicado
-        offset: '450px;'
+        offset: '440px;'
 })
+
+$('div#transicao-testemunha-plano').waypoint(
+    function(direcao) {
+        if (direcao == "down") {
+            $('div#decoracao1').removeClass('animate__animated animate__fadeOutLeft')
+            $('div#decoracao3').removeClass('animate__animated animate__fadeOutRight')
+
+            $('div#decoracao1').addClass('animate__animated animate__fadeInLeft')
+            $('div#decoracao2').addClass('animate__animated animate__fadeInUp')
+            $('div#decoracao3').addClass('animate__animated animate__fadeInRight')
+        } if(direcao == "up") {
+            $('div#decoracao1').removeClass('animate__animated animate__fadeInLeft')
+            $('div#decoracao3').removeClass('animate__animated animate__fadeInRight')
+        }
+    }, {
+        // Indica quantos pixels antes do elemento o efeito começará a ser aplicado
+        offset: '560px;'
+})
+
+$('div#transicao-testemunha-plano').waypoint(
+    function(direcao) {
+        if(direcao == "up") {
+            $('div#decoracao1').addClass('animate__animated animate__fadeOutLeft')
+            $('div#decoracao3').addClass('animate__animated animate__fadeOutRight')
+        }
+    }, {
+        // Indica quantos pixels antes do elemento o efeito começará a ser aplicado
+        offset: '-190px;'
+})
+
+$('section#plano-principal').waypoint(
+    function(direcao) {
+        if (direcao == "down") {
+            $('div.planos1').removeClass('animate__animated animate__fadeOutLeft')
+            $('div.planos2').removeClass('animate__animated animate__fadeOutDown')
+            $('div.planos3').removeClass('animate__animated animate__fadeOutRight')
+
+            $('div.planos1').addClass('animate__animated animate__fadeInLeft')
+            $('div.planos2').addClass('animate__animated animate__fadeInUp')
+            $('div.planos3').addClass('animate__animated animate__fadeInRight')
+        } if (direcao == "up") {
+            $('div.planos1').removeClass('animate__animated animate__fadeInLeft')
+            $('div.planos2').removeClass('animate__animated animate__fadeInUp')
+            $('div.planos3').removeClass('animate__animated animate__fadeInRight')
+        }
+    }, {
+        // Indica quantos pixels antes do elemento o efeito começará a ser aplicado
+        offset: '250px;'
+})
+
+$('section#plano-principal').waypoint(
+    function(direcao) {
+         if (direcao == "up") {
+            $('div.planos1').addClass('animate__animated animate__fadeOutLeft')
+            $('div.planos2').addClass('animate__animated animate__fadeOutDown')
+            $('div.planos3').addClass('animate__animated animate__fadeOutRight')
+        }
+    }, {
+        // Indica quantos pixels antes do elemento o efeito começará a ser aplicado
+        offset: '-300px;'
+})
+
 
 function mostrarMenu() {
     $('nav ul#esquerda').css('display', 'flex');
