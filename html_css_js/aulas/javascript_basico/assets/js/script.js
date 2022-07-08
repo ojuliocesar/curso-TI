7e1a8de4fbd2c21c9ef2d6690938bef03e9f9d8e
ojuliocesar <97842753+ojuliocesar@users.noreply.github.com>
2022-07-07 22:30:22 -0300

    Add onchange

--- html_css_js/aulas/javascript_basico/assets/js/script.js
+++ html_css_js/aulas/javascript_basico/assets/js/script.js
@@ -1,5 +1,4 @@
 const soma = () => {
-
     let numero1 = prompt('Digite o primeiro número')
 
     let numero2 = prompt('Digite o segundo número')
@@ -29,4 +28,61 @@ const somaNumeros = () => {
     // Exibe a soma dos números dentro da div resultado
 
     document.getElementById('resultado').innerHTML = `Resultado da soma é ${soma}`
+}
+
+const exibe = (id) => {
+    let elemento =  document.getElementById(id)
+
+    if (elemento.hidden == true) {
+        elemento.hidden = false
+    } else {
+        elemento.hidden = true
+    } 
+}
+
+// const exibeSelect = () => {
+//     let opcao = document.getElementById('tecnologia').value
+
+//     let elemento =  document.getElementById(opcao)
+
+//     if (elemento.hidden == true) {
+//         elemento.hidden = false
+//     } else {
+//         elemento.hidden = true
+//     } 
+// }
+
+const exibeCidade = () => {
+    let estado = document.getElementById('estado')
+
+    let cidade = document.getElementById('cidade')
+
+    if (estado.value == 'sp') {
+        cidade.innerHTML = `
+            <option value="">São João da Boa Vista</option>
+            <option value="">Aguaí</option>
+            <option value="">Águas da Prata</option>
+        `
+    }else if (estado.value == 'rj') {
+        cidade.innerHTML = `
+            <option value="">Laranjeiras</option>
+            <option value="">Xerém</option>
+            <option value="">Parati</option>
+        `
+    }else if (estado.value == 'mg') {
+        cidade.innerHTML = `
+            <option value="">Andradas</option>
+            <option value="">Poços de Caldas</option>
+            <option value="">Belo Horizonte</option>
+        `
+    }
+}
+
+// Função que consulta o cep de uma API
+// API utilizada: POSTMON
+
+const consultaCEP= () => {
+    let cep = document.getElementById('cep').value
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
 }
